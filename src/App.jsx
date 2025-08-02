b429a15d7b741959492fea116dc11205fdd9e949
CHAPPIDI LOWKHYA NANDINI REDDY <163741011+2300033036@users.noreply.github.com>
2025-08-02 11:45:35 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ByLowkhya">
+    <BrowserRouter basename="/WEBApp">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
